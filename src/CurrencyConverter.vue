c40e48eb2af054487031fa3fcefefba86f19f40c
Xabi <xabier.etxeberria@orekainteractive.com>
2021-01-21 16:04:54 +0100

    Input for value to convert

--- src/CurrencyConverter.vue
+++ src/CurrencyConverter.vue
@@ -1,12 +1,22 @@
 <template>
 	<div id="currency-converter-app">
-
+		<input type="text" maxlength="9" v-model="valueToConvert" @input="checkInput" />
+		<input type="button" value="Convert" />
 	</div>
 </template>
 
 <script>
 export default {
-	
+	data() {
+		return {
+			valueToConvert: 0
+		}
+	},
+	methods: {
+		checkInput() {
+			console.log(this.valueToConvert);
+		}
+	}
 }
 </script>
 

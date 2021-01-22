c8b9294c71ed0a6daf5e81333f31c86c45c18589
Xabi <xabier.etxeberria@orekainteractive.com>
2021-01-22 08:34:09 +0100

    Sorted drop-down lists for currencies

--- src/CurrencyConverter.vue
+++ src/CurrencyConverter.vue
@@ -1,7 +1,16 @@
 <template>
 	<div id="currency-converter-app">
 		<input type="text" maxlength="9" v-model="valueToConvert" @input="checkInput" />
-		<input type="button" value="Convert" />
+		<select id="currency-from">
+			<option v-for="c in currencies" :key="c.code" :value="c.code">{{ c.name }}</option>
+		</select>
+
+		<br/><br/>
+
+		<input type="text" v-model="resultValue" />
+		<select id="currency-to">
+			<option v-for="c in currencies" :key="c.code" :value="c.code">{{ c.name }}</option>
+		</select>
 	</div>
 </template>
 
@@ -9,13 +18,23 @@
 export default {
 	data() {
 		return {
-			valueToConvert: 0
+			valueToConvert: 0,
+			currencies: [
+				{ name: 'Euro', code: 'eur' },
+				{ name: 'US Dollar', code: 'usd' },
+				{ name: 'Yen', code: 'jpy' }
+			].sort(this.compare)
 		}
 	},
 	methods: {
+		compare(a, b) {
+			if (a.code < b.code) return -1;
+			if (a.code > b.code) return 1;
+			return 0;
+		},
 		checkInput() {
 			console.log(this.valueToConvert);
-		}
+		},
 	}
 }
 </script>
